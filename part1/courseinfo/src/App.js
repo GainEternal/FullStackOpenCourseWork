2d14af2c712bda85ae208e9f6b55491e85581d26
GainEternal <84601209+GainEternal@users.noreply.github.com>
2022-09-01 08:15:59 -0700

    Exercise 1.2
    in part1/courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,14 +9,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
       <p>
-        {props.part[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]} {props.exercises[2]}
+        {props.part} {props.exercises}
       </p>
     </>
   )
