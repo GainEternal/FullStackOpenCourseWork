5f08d5b24fc78d9489cbbf11ddc7e148d6c033ce
GainEternal <84601209+GainEternal@users.noreply.github.com>
2022-09-15 09:19:09 -0700

    Finished Exercise 2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Course = ({ course }) => 
+  {const total = course.parts.reduce((sum, next) => sum + next.exercises, 0)
+    return <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={total} />
+      <br></br>
+    </div>
+  }
+
+const Header = ({ courseName }) => <h2>{courseName}</h2>
+
+const Total = ({ sum }) => <p><b>Total Number of Exercises: {sum} </b></p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name}: {part.exercises}
+  </p>
+
+const Content = ({ parts }) =>
+  parts.map(part => <Part part={part} />)
+
+export default Course
