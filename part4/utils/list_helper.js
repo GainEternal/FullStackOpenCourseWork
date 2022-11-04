35808908feaf04cea7b358726efe2b5330a260e6
GainEternal <84601209+GainEternal@users.noreply.github.com>
2022-11-04 12:09:03 -0700

    Ex 4.5 - favoriteBlog

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,30 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = ( blogs ) => {
+  if (!Array.isArray(blogs) || !blogs.length) {
+    return 0
+  }
+  const total = blogs.reduce(
+    ( sum, blog ) => sum + blog.likes,
+    0
+  )
+  return total
+}
+
+const favoriteBlog = ( blogs ) => {
+  if (!Array.isArray(blogs) || !blogs.length) {
+    return {}
+  }
+  const blogWithMostLikes = blogs.reduce(
+    (max, current) => (current.likes > max.likes) ? current : max 
+  )
+  const blogInfo = (({ title, author, likes }) => ({ title, author, likes }))(blogWithMostLikes)
+  return blogInfo
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
